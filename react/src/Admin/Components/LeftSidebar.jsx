fe855376a74a2e1231979db08e16ab59b1d1bb0a
tinhle2607 <131324115+tinhle2607@users.noreply.github.com>
2025-01-12 07:31:58 +0700

    rềactor : edit linklink

--- react/src/Admin/Components/LeftSidebar.jsx
+++ react/src/Admin/Components/LeftSidebar.jsx
@@ -6,7 +6,7 @@ const LeftSidebar = ({ onSidebarToggle }) => {
   const sidebarItems = [
     { type: 'section', label: 'Trang chủ', icon: 'ti ti-dots' },
     { type: 'link', label: 'Doanh Thu', path: '/admin', icon: 'mdi:view-dashboard' },
-    { type: 'link', label: 'Thông tin trang chủ', path: '/menu', icon: 'solar:home-smile-bold-duotone' },
+    { type: 'link', label: 'Thông tin trang chủ', path: '/admin/menu', icon: 'solar:home-smile-bold-duotone' },
     { type: 'section', label: 'Quản lý', icon: 'ti ti-dots' },
     { type: 'link', label: 'Tài khoản hệ thống', path: '/admin/system-accounts', icon: 'mdi:account-group' },
     // { type: 'link', label: 'Hợp đồng', path: '/admin/contracts', icon: 'clarity:contract-solid' },
